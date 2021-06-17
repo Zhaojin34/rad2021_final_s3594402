0d0e016669982545e728908f4d0a574d4f3e36d6
Zhaojin34 <s3594402@student.rmit.edu.au>
2021-06-17 21:50:24 +0000

    initialize

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Quiz.create(
+    "id":814,
+    question:"How to remove unused data (all unused containers, networks, images (both dangling and unreferenced), and optionally, volumes) in Docker.,",
+    description:null,
+    answers:{
+      answer_a:"docker system --prune",
+      answer_b:"docker system prune sys",
+      answer_c:"docker system prune",
+      answer_d:null,
+      answer_e:null,
+      answer_f:null
+    },
+    multiple_correct_answers:"false",
+    correct_answers:{
+      answer_a_correct:"false",
+      answer_b_correct:"false",
+      answer_c_correct:"true",
+      answer_d_correct:"false",
+      answer_e_correct:"false",
+      answer_f_correct:"false"
+    },
+    correct_answer:"answer_a",
+    explanation:null,
+    tip:null,
+    tags:[{"name":"Docker"}],
+    category:"Docker",
+    difficulty:"Medium"
+)
+
+History.create(
+  score: "3",
+  number: "4"
+)
